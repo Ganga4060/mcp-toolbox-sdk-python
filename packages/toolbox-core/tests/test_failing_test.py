d432cffa1a38c830349c9c78d859763665d02fd6
Ganga4060 <nainigangothri11@gmail.com>
2025-10-26 05:58:21 +0000

    feat:updated test for running workflow

--- packages/toolbox-core/tests/test_failing_test.py
+++ packages/toolbox-core/tests/test_failing_test.py
@@ -6,7 +6,7 @@ def test_failing_with_logs():
     """
     This test intentionally fails and prints logs to stdout and stderr and test is for step3 and step4 of the workflow now updated to run.
     """
-    print("[LOG] This is a test log message to stdout.")
+    print("[LOG] This is a test log message to stdout updated test.")
     print("[ERROR] Simulated error before failure.", file=sys.stderr)
     print("[WARN] This is a warning log.")
     print("[DEBUG] Debugging details here.")
