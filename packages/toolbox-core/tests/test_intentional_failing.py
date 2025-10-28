a201b6d0ccaedb7ac556e2a0e948bbbff5c3db96
Ganga4060 <nainigangothri11@gmail.com>
2025-10-28 07:14:53 +0000

    feat: testing triggers

--- packages/toolbox-core/tests/test_intentional_failing.py
+++ packages/toolbox-core/tests/test_intentional_failing.py
@@ -2,7 +2,7 @@ def intentional_fail_test():
     """This test is intentionally failing to validate that :
     1. Cloud Build checks fail as expected or not.
     2. The export_build_logs workflow detects the failure 
-    3. Failed check information is correctly extracted or not 
+    3. Failed check information is correctly extracted or not2 
 
     """
     assert False, "Intentional failure to test Exporting Cloud Build Failure Logs"
