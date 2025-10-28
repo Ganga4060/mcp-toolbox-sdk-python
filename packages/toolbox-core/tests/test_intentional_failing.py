a81e50f7074a703a089be6ffacb10b2684f00fbc
Ganga4060 <nainigangothri11@gmail.com>
2025-10-28 07:54:59 +0000

    feat: checking the triggers are running or not

--- packages/toolbox-core/tests/test_intentional_failing.py
+++ packages/toolbox-core/tests/test_intentional_failing.py
@@ -1,5 +1,6 @@
 def intentional_fail_test():
     """This test is intentionally failing to validate that :
+    
     1. Cloud Build checks fail as expected or not.
     2. The export_build_logs workflow detects the failure 
     3. Failed check information is correctly extracted or not2 
