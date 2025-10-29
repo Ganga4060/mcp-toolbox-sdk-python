c1ac22af50275624667523f5ee8b8fb6fd7fa484
Ganga4060 <nainigangothri11@gmail.com>
2025-10-29 12:06:36 +0000

    feat: updated test

--- packages/toolbox-core/tests/test_failing.py
+++ packages/toolbox-core/tests/test_failing.py
@@ -2,4 +2,4 @@ import pytest
 
 
 def test_intentional_failure():
-    assert False, "This test is designed to fail for CI edge case testing."
+    assert False, "This test is designed to fail for CI edge case testing...."
