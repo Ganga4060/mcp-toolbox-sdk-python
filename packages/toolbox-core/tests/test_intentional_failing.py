e8180cf7514242f4a4af84725cb9bce79b072874
Ganga4060 <nainigangothri11@gmail.com>
2025-10-29 06:35:15 +0000

    feat: commenting on pr

--- packages/toolbox-core/tests/test_intentional_failing.py
+++ packages/toolbox-core/tests/test_intentional_failing.py
@@ -3,7 +3,7 @@ def intentional_failing_test1():
     
     1. Cloud Build checks fail as expected or not.
     2. The export_build_logs workflow detects the failure 
-    3. Failed check information is correctly extracted or not.
+    added workflow for pr commenting
 
     """
     assert False, "Intentional failure to test Export Cloud Build Failure Logs...."
