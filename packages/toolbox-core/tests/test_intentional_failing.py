c3ea32c4c0055582a0d5599dde301c16abf9f426
Ganga4060 <nainigangothri11@gmail.com>
2025-10-29 09:59:32 +0000

    feat: testing edge cases

--- packages/toolbox-core/tests/test_intentional_failing.py
+++ packages/toolbox-core/tests/test_intentional_failing.py
@@ -3,7 +3,7 @@ def intentional_fail():
     1. Cloud Build checks fail as expected
     2. The export_build_logs workflow detects the failure
     3. Failed check information is correctly extracted.
-    4.removed sample pr commenting code file
+    4.removed sample pr commenting code file and added edge cases.
 
     """
     assert False, "Intentional failure to test export_build_logs workflow"
